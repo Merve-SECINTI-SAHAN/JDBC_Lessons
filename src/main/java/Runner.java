12b816f5682c5017650e6cc5d667f5720d398a1f
Merve Seçinti Şahan <secintimerve27@gmail.com>
2023-06-20 00:15:58 +0300

    20-06-2023

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,7 +1,6 @@
 public class Runner {
     public static void main(String[] args) {
         // Databese'e baglan
-
         JDBCUtils.connectToDataBase();
 
         //Statement olustur
@@ -10,6 +9,8 @@ public class Runner {
         // query calistir.
         String sql = "CREATE TABLE workers(worker_id varchar(20), worker_name varchar(20), worker_salary int);";
         JDBCUtils.execute(sql);
+
+
 
 
     }
